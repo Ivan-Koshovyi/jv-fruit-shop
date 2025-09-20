68bf64dcc1ac985c86574f41e6ce98a7aca4c099
ivankosh <ivan2023koshovyi@gmail.com>
2025-09-20 21:51:35 +0300

    add ReturnOperation.java for operations

---
+++ src/main/java/core/basesyntax/operations/ReturnOperation.java
@@ -0,0 +1,12 @@
+package core.basesyntax.operations;
+
+import core.basesyntax.FruitTransaction;
+
+import java.util.Map;
+
+public class ReturnOperation implements OperationHandler {
+    @Override
+    public void handle(FruitTransaction transaction, Map<String, Integer> storage) {
+        storage.put(transaction.getFruit(), storage.getOrDefault(transaction.getFruit(), 0) + transaction.getQuantity());
+    }
+}
