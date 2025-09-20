a6ed704459552b575ec2186beef63077eed91c97
ivankosh <ivan2023koshovyi@gmail.com>
2025-09-20 21:43:54 +0300

    implement DataConverterImpl to convert CSV lines to FruitTransaction objects

---
+++ src/main/java/core/basesyntax/converter/DataConverterImpl.java
@@ -0,0 +1,31 @@
+package core.basesyntax.converter;
+
+import core.basesyntax.FruitTransaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataConverterImpl implements DataConverter {
+    @Override
+    public List<FruitTransaction> convertToTransaction(List<String> reportToReadFruits) {
+        List<FruitTransaction> transactions =new ArrayList<>();
+        for (String report : reportToReadFruits) {
+            String[] reportSplit = report.split(",");
+            String code = reportSplit[0];
+            String fruit = reportSplit[1];
+            int quantity = Integer.parseInt(reportSplit[2]);
+            FruitTransaction.Operation operationCode = null;
+            for (FruitTransaction.Operation op : FruitTransaction.Operation.values()) {
+                if (op.getCode().equals(code)) {
+                    operationCode = op;
+                    break;
+                }
+            }
+            if (operationCode != null) {
+                FruitTransaction fruitTransaction = new FruitTransaction(operationCode, fruit, quantity);
+                transactions.add(fruitTransaction);
+            }
+        }
+        return transactions;
+    }
+}
