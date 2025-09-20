bec301dc86c14693ceb1f6717457a209b0fb58a8
ivankosh <ivan2023koshovyi@gmail.com>
2025-09-20 21:56:07 +0300

    Implement processing pipeline: read CSV, convert to transactions, apply operations

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,40 @@
+package core.basesyntax;
+
+import core.basesyntax.converter.DataConverter;
+import core.basesyntax.converter.DataConverterImpl;
+import core.basesyntax.file.ReadFile;
+import core.basesyntax.file.ReadFileImpl;
+import core.basesyntax.operations.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        ReadFile reportOfFile = new ReadFileImpl();
+        List<String> reportToReadFruits = reportOfFile.read("reportToRead.csv");
+
+        DataConverter dataConverter = new DataConverterImpl();
+        List<FruitTransaction> transactions = dataConverter.convertToTransaction(reportToReadFruits);
+
+        Map<FruitTransaction.Operation, OperationHandler> operationHandlers = new HashMap<>();
+        operationHandlers.put(FruitTransaction.Operation.BALANCE, new BalanceOperation());
+        operationHandlers.put(FruitTransaction.Operation.PURCHASE, new PurchaseOperation());
+        operationHandlers.put(FruitTransaction.Operation.RETURN, new ReturnOperation());
+        operationHandlers.put(FruitTransaction.Operation.SUPPLY, new SupplyOperation());
+        OperationStrategy operationStrategy = new OperationStrategyImpl(operationHandlers);
+
+        ShopService shopService = new ShopServiceImpl(operationStrategy);
+        shopService.process(transactions);
+
+        // 5.Generate report based on the current Storage state
+        ReportGenerator reportGenerator = new ReportGeneratorImpl();
+        String resultingReport = reportGenerator.getReport();
+
+        // 6. Write the received report into the destination file
+//        FileWriter fileWriter = new FileWriterImpl();
+//        fileWriter.write(resultingReport, "finalReport.csv");
+
+    }
+}
